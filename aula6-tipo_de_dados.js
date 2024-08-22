0256b144739c4093276d3fadb739e52ca3a1131e
Yasmin de França <144808719+yasmin-franca@users.noreply.github.com>
2024-08-22 19:24:01 -0300

    Update aula6-tipo_de_dados.js

--- aula6-tipo_de_dados.js
+++ aula6-tipo_de_dados.js
@@ -53,3 +53,79 @@ tipo de dados para representar valor verdadeiro ou falso
 comparações resultam em boleanos
 é possível atribuir um true/false a uma variável e ela terá o tipo boolean
 */
+var verdadeiro = true;
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+var falso = false;
+console.log(falso);
+console.log(typeof falso);
+
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+Undefined e null:
+null é tipo de dado que representa um valor
+undefined é um tipo de dado para uma varíavel com valor não atribuído
+*/
+var nome = null;
+console.log(nome);
+console.log(typeof nome);
+
+// a few moments later |
+
+nome = "myg";
+console.log(nome);
+console.log(typeof nome);
+
+var sobrenome2;
+console.log(sobrenome2);
+console.log(typeof sobrenome2);
+
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+Object:
+funcionam como um array associativo de outras linguagens
+pode criar propriedades com chave e valor
+idea de guardar um conjunto de valores para usar depois
+*/
+var obj = {
+    nome: "MYG",
+    ramo: "empresa",
+    tempo_de_mercado: 40,
+    esta_ativa: true,
+};
+console.log(obj);
+console.log(typeof obj);
+
+//acessando propriedades do objeto
+console.log(obj.nome);
+console.log(obj.ramo);
+console.log(obj.tempo_de_mercado);
+
+console.log("O nome da empresa é: " + obj.nome);
+
+/*----------------------------------------------------------------------------------------------------*/
+
+/*
+Arrays:
+São considerados objetos, mas servem como listas
+podem ter itens de qualquer tipod de dado
+Porém, não por chave e valor, e sim por índice
+*/
+var arr = ["MYG", "empresa", 40, true, {teste: 1, teste: 2}];
+console.log(arr);
+
+var arr2 = [2,3,4,5,6,7]; //mais comum arrays com um tipo só.
+console.log(arr2);
+
+//itens do array
+console.log(arr[1]);
+console.log(arr2[0]);
+
+//inserindo valores ao array
+arr[4] = false;
+console.log(arr);
+
+console.log(typeof arr);
