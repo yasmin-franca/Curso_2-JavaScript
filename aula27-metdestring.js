614ce14d9ba7b87cb41fe20ffa52ed8f743fbd80
Yasmin de França <144808719+yasmin-franca@users.noreply.github.com>
2024-09-13 14:58:38 -0300

    Update aula27-metdestring.js

--- aula27-metdestring.js
+++ aula27-metdestring.js
@@ -15,4 +15,36 @@ console.log(roeu);
 var novaFrase = frase.replace("roeu", "comeu");
 console.log(novaFrase);
 
+
+
 //métodos que podem modificar
+//toUpperCase e toLowerCase
+var frase = "Esta é a frase que vamos manipular"
+
+var fraseCaixaAlta = frase.toUpperCase();
+
+console.log(fraseCaixaAlta);
+
+console.log(fraseCaixaAlta.toLowerCase());
+
+//trim - tirar espaço em excesso
+var nome = "     Matheus       "
+
+var nomeTrim = nome.trim();
+
+console.log(nome);
+console.log(nomeTrim);
+
+//split
+console.log(frase.split(" "));
+
+var tags = "PHP, JavaScript, HTML, CSS";
+
+console.log(tags.split(", "));
+
+//lastIndexOf
+var fraseDois = "Eu quero a última palavra teste dessa frase de teste";
+
+console.log(fraseDois.indexOf("teste"));
+
+console.log(fraseDois.lastIndexOf("teste"));
